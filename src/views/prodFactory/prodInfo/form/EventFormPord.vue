8effe8bd3ea82de36b30982e9f7fdffb6245b845
chengliang <chengliang@dcits.com>
2018-08-07 19:57:43 +0800

    增加产品明细

--- src/views/prodFactory/prodInfo/form/EventFormPord.vue
+++ src/views/prodFactory/prodInfo/form/EventFormPord.vue
@@ -3,105 +3,122 @@
     <v-card ref="form">
       <v-card-text>
         <v-layout row wrap>
-          <v-flex xs2 class="pr-0 mr-0"><v-subheader class="subheading">产品类型</v-subheader></v-flex>
-          <v-flex xs4>
-            <v-text-field label="产品类型" name="title" v-model="prodtypeData" prepend-icon="check_circle">
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">产品类型 *</span>
+            <v-text-field label="产品类型" name="title" v-model="prodtypeData" single-line>
             </v-text-field>
           </v-flex>
-          <v-flex xs2/>
-          <v-flex xs4>
-            <v-text-field label="产品描述" name="title" v-model="proddescData" color="cyan darken" />
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">产品描述</span>
+            <v-text-field label="产品描述" name="title" v-model="proddescData" single-line/>
           </v-flex>
-          <v-flex xs1/>
+          <!-- <v-flex xs1/> -->
         </v-layout>
         <v-layout row wrap>
-          <v-flex xs1/>
-          <v-flex xs4>
-            <v-select :items="busimodel" v-model="busimodelData" label="业务模块" item-text="prodDesc" item-value="prodCode" prepend-icon="check_circle"></v-select>
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">业务模块</span>
+            <v-select :items="busimodel" v-model="busimodelData" label="业务模块" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs2/>
-          <v-flex xs4>
-            <v-select :items="prodclass" v-model="prodclassData" label="产品分类" item-text="prodDesc" item-value="prodCode"></v-select>
+          <!-- <v-flex xs2/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">产品分类</span>
+            <v-select :items="prodclass" v-model="prodclassData" label="产品分类" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs1/>
+          <!-- <v-flex xs1/> -->
         </v-layout>
         <v-layout row wrap>
-          <v-flex xs1/>
-          <v-flex xs4>
-            <v-select :items="prodprepice" v-model="prodprepiceData" label="产品属性" item-text="prodDesc" item-value="prodCode"></v-select>
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">产品属性</span>
+            <v-select :items="prodprepice" v-model="prodprepiceData" label="产品属性" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs2/>
-          <v-flex xs4>
-            <v-select :items="prodmuti" v-model="prodmutiData" label="组合产品" item-text="prodDesc" item-value="prodCode"></v-select>
+          <!-- <v-flex xs2/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">组合产品</span>
+            <v-select :items="prodmuti" v-model="prodmutiData" label="组合产品" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs1/>
+          <!-- <v-flex xs1/> -->
         </v-layout>
         <v-layout row wrap>
-          <v-flex xs1/>
-          <v-flex xs4>
-            <v-select :items="prodstatus" v-model="prodstatusData" label="产品状态" item-text="prodDesc" item-value="prodCode"></v-select>
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">产品状态</span>
+            <v-select :items="prodstatus" v-model="prodstatusData" label="产品状态" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs2/>
-          <v-flex xs4>
-            <v-select :items="baseprod" v-model="baseprodData" label="基础产品" item-text="prodDesc" item-value="prodCode" prepend-icon="check_circle"></v-select>
+          <!-- <v-flex xs2/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">基础产品 *</span>
+            <v-select :items="baseprod" v-model="baseprodData" label="基础产品" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs1/>
+          <!-- <v-flex xs1/> -->
         </v-layout>
         <v-layout row wrap>
-          <v-flex xs1/>
-          <v-flex xs4>
-            <v-select :items="accttype" v-model="accttypeData" label="账户类型" item-text="prodDesc" item-value="prodCode"></v-select>
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">账户类型</span>
+            <v-select :items="accttype" v-model="accttypeData" label="账户类型" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs2/>
-          <v-flex xs4>
-            <v-select :items="acctsontype" v-model="acctsontypeData" label="账户子类型" item-text="prodDesc" item-value="prodCode" prepend-icon="check_circle"></v-select>
+          <!-- <v-flex xs2/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">账户子类型</span>
+            <v-select :items="acctsontype" v-model="acctsontypeData" label="账户子类型" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs1/>
+          <!-- <v-flex xs1/> -->
         </v-layout>
         <v-layout row wrap>
-          <v-flex xs1/>
-          <v-flex xs4>
-            <v-select :items="acctstruct" v-model="acctstructData" label="账户结构" item-text="prodDesc" item-value="prodCode" prepend-icon="check_circle"></v-select>
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">账户结构</span>
+            <v-select :items="acctstruct" v-model="acctstructData" label="账户结构" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs2/>
-          <v-flex xs4>
-            <v-select :items="virtualflag" v-model="virtualflagData" label="虚实标志" item-text="prodDesc" item-value="prodCode" prepend-icon="check_circle"></v-select>
+          <!-- <v-flex xs2/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">虚实标志</span>
+            <v-select :items="virtualflag" v-model="virtualflagData" label="虚实标志" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs1/>
+          <!-- <v-flex xs1/> -->
         </v-layout>
         <v-layout row wrap>
-          <v-flex xs1/>
-          <v-flex xs4>
-            <v-select :items="acctintflag" v-model="acctintflagData" label="账户计息标志" item-text="prodDesc" item-value="prodCode" prepend-icon="check_circle"></v-select>
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">账户计息标志</span>
+            <v-select :items="acctintflag" v-model="acctintflagData" label="账户计息标志" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs2/>
-          <v-flex xs4>
-            <v-select :items="amtflag" v-model="amtflagData" label="金额标志" item-text="prodDesc" item-value="prodCode"></v-select>
+          <!-- <v-flex xs2/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">金额标志</span>
+            <v-select :items="amtflag" v-model="amtflagData" label="金额标志" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
-          <v-flex xs1/>
+          <!-- <v-flex xs1/> -->
         </v-layout>
         <v-layout row wrap>
-          <v-flex xs1/>
-          <v-flex xs4>
-            <v-select :items="profitcenter" v-model="profitcenterData" label="利润中心" item-text="prodDesc" item-value="prodCode"></v-select>
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">利润中心</span>
+            <v-select :items="profitcenter" v-model="profitcenterData" label="利润中心" item-text="prodDesc" item-value="prodCode" single-line></v-select>
           </v-flex>
         </v-layout>
         <v-layout row wrap>
-          <v-flex xs1/>
-          <v-flex xs4>
+          <!-- <v-flex xs1/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">生效日期</span>
             <v-menu class="pr-2" ref="statDate" lazy :close-on-content-click="false" v-model="startDateMenu" transition="scale-transition" offset-y full-width :nudge-bottom="-22" max-width="290px" :return-value.sync="startDate">
-              <v-text-field slot="activator" label="生效日期" v-model="startDate" append-icon="event" readonly prepend-icon="check_circle"></v-text-field>
-              <v-date-picker v-model="startDate" no-title scrollable >
+              <v-text-field slot="activator" label="生效日期" v-model="startDate" append-icon="event" single-line></v-text-field>
+              <v-date-picker v-model="startDate" no-title scrollable>
                 <v-spacer></v-spacer>
                 <v-btn flat color="primary" @click="startDateMenu = false">Cancel</v-btn>
                 <v-btn flat color="primary" @click="$refs.statDate.save(startDate)">OK</v-btn>
               </v-date-picker>
             </v-menu>
           </v-flex>
-          <v-flex xs2/>
-          <v-flex xs4>
+          <!-- <v-flex xs2/> -->
+          <v-flex xs6 class="field-single-line">
+            <span class="primary--text mx-3 pt-4 subheading">失效日期</span>
             <v-menu ref="endDate" lazy class="pr-2" :close-on-content-click="false" v-model="endDateMenu" transition="scale-transition" offset-y full-width :nudge-bottom="-22" min-width="290px" :return-value.sync="endDate">
-              <v-text-field slot="activator" label="失效日期" v-model="endDate" append-icon="event" readonly prepend-icon="check_circle"></v-text-field>
+            <v-text-field slot="activator" label="失效日期" v-model="endDate" append-icon="event" single-line></v-text-field>
               <v-date-picker v-model="endDate" no-title scrollable>
                 <v-spacer></v-spacer>
                 <v-btn flat color="primary" @click="endDateMenu = false">Cancel</v-btn>
@@ -109,7 +126,7 @@
               </v-date-picker>
             </v-menu>
           </v-flex>
-          <v-flex xs1/>
+          <!-- <v-flex xs1/> -->
         </v-layout>
         <!-- </v-form> -->
       </v-card-text>
