8a64b51121590706cacb688d3d0d10690c20046f
chengliang <chengliang@dcits.com>
2018-08-08 17:38:39 +0800

    产品目录字体大小

--- src/views/prodFactory/prodFlow/tableFlow/prodListCmb.vue
+++ src/views/prodFactory/prodFlow/tableFlow/prodListCmb.vue
@@ -7,7 +7,7 @@
         <v-divider></v-divider>
         <v-card-text class="pa-0">
             <v-data-table :headers="complex.headers" :search="search" :items="items" :rows-per-page-items="[10,25,50,{text:'All','value':-1}]" class="elevation-1" item-key="prodCode" select-all v-model="complex.selected">
-                <template slot="items" slot-scope="props" class="body-2">
+                <template slot="items" slot-scope="props">
                                                           <td>
                                                             <v-checkbox primary hide-details v-model="props.selected"></v-checkbox>
                                                           </td>
