cd735d2a2b21f28d6816e1a2ff9165431a7c25e8
chengliang <chengliang@dcits.com>
2018-08-13 15:28:31 +0800

    增加switch开关 multiple 多选组件使用样例

--- src/views/prodFactory/prodInfo/form/EventFormPord.vue
+++ src/views/prodFactory/prodInfo/form/EventFormPord.vue
@@ -18,42 +18,43 @@
               </v-flex>
               <v-flex md4 lg4>
                 <!-- <span class="primary--text mx-3 pt-4 subheading">产品描述</span> -->
-                <v-text-field class="primary--text mx-2 " label="产品描述" name="title" v-model="proddescData" single-line hide-details/>
+                <v-text-field class="primary--text mx-2" label="产品描述" name="title" v-model="proddescData" single-line hide-details/>
               </v-flex>
               <v-flex md2 lg2>
                 <v-subheader class="primary--text pt-3 subheading">业务模块*</v-subheader>
               </v-flex>
               <v-flex md4 lg4>
                 <!-- <span class="primary--text mx-3 pt-4 subheading">业务模块</span> -->
-                <v-select class="primary--text mx-2 " :items="busimodel" v-model="busimodelData" label="业务模块" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
+                <v-select class="primary--text mx-2" :items="busimodel" v-model="busimodelData" label="业务模块" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
               </v-flex>
               <v-flex md2 lg2>
                 <v-subheader class="primary--text pt-3 subheading">产品分类*</v-subheader>
               </v-flex>
               <v-flex md4 lg4>
                 <!-- <span class="primary--text mx-3 pt-4 subheading">产品分类</span> -->
-                <v-select class="primary--text mx-2 " :items="prodclass" v-model="prodclassData" label="产品分类" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
+                <v-select class="primary--text mx-2" :items="prodclass" v-model="prodclassData" label="产品分类" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
               </v-flex>
               <v-flex md2 lg2>
                 <v-subheader class="primary--text pt-3 subheading">产品属性*</v-subheader>
               </v-flex>
               <v-flex md4 lg4>
                 <!-- <span class="primary--text mx-3 pt-4 subheading">产品属性</span> -->
-                <v-select class="primary--text mx-2 " :items="prodprepice" v-model="prodprepiceData" label="产品属性" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
+                <v-select class="primary--text mx-2" :items="prodprepice" v-model="prodprepiceData" label="产品属性" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
               </v-flex>
               <v-flex md2 lg2>
                 <v-subheader class="primary--text pt-3 subheading">组合产品*</v-subheader>
               </v-flex>
               <v-flex md4 lg4>
                 <!-- <span class="primary--text mx-3 pt-4 subheading">组合产品</span> -->
-                <v-select class="primary--text mx-2 " :items="prodmuti" v-model="prodmutiData" label="组合产品" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
+                <!-- <v-select class="primary--text mx-2" :items="prodmuti" v-model="prodmutiData" label="组合产品" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select> -->
+                <v-switch :label="`${prodmutiData==='Y'?'是':'否'}`" v-model="prodmutiData" value="Y" color="success" hide-details></v-switch>
               </v-flex>
               <v-flex md2 lg2>
                 <v-subheader class="primary--text pt-3 subheading">产品状态*</v-subheader>
               </v-flex>
               <v-flex md4 lg4>
                 <!-- <span class="primary--text mx-3 pt-4 subheading">产品状态</span> -->
-                <v-select class="primary--text mx-2 " :items="prodstatus" v-model="prodstatusData" label="产品状态" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
+                <v-select class="primary--text mx-2" :items="prodstatus" v-model="prodstatusData" label="产品状态" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
               </v-flex>
               <v-flex md2 lg2>
                 <v-subheader class="primary--text pt-3 subheading">基础产品*</v-subheader>
@@ -67,7 +68,7 @@
               </v-flex>
               <v-flex md4 lg4>
                 <!-- <span class="primary--text mx-3 pt-4 subheading">账户类型</span> -->
-                <v-select class="primary--text mx-2 " :items="accttype" v-model="accttypeData" label="账户类型" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
+                <v-select class="primary--text mx-2" :items="accttype" v-model="accttypeData" label="账户类型" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
               </v-flex>
               <v-flex md2 lg2>
                 <v-subheader class="primary--text pt-3 subheading">账户子类型*</v-subheader>
@@ -81,7 +82,7 @@
               </v-flex>
               <v-flex md4 lg4>
                 <!-- <span class="primary--text mx-3 pt-4 subheading">账户结构</span> -->
-                <v-select class="primary--text mx-2 " :items="acctstruct" v-model="acctstructData" label="账户结构" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
+                <v-select class="primary--text mx-2" :items="acctstruct" v-model="acctstructData" label="账户结构" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
               </v-flex>
               <v-flex md2 lg2>
                 <v-subheader class="primary--text pt-3 subheading">虚实标志*</v-subheader>
@@ -98,11 +99,11 @@
                 <v-select class="primary--text mx-2" :items="acctintflag" v-model="acctintflagData" label="计息标志" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
               </v-flex>
               <v-flex md2 lg2>
-                <v-subheader class="primary--text pt-3 subheading">产品类型*</v-subheader>
+                <v-subheader class="primary--text pt-3 subheading">金额标志*</v-subheader>
               </v-flex>
               <v-flex md4 lg4>
                 <!-- <span class="primary--text mx-3 pt-4 subheading">金额标志</span> -->
-                <v-select class="primary--text mx-2 " :items="amtflag" v-model="amtflagData" label="金额标志" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
+                <v-select class="primary--text mx-2" :items="amtflag" v-model="amtflagData" label="金额标志" item-text="prodDesc" item-value="prodCode" single-line hide-details></v-select>
               </v-flex>
               <v-flex md2 lg2>
                 <v-subheader class="primary--text pt-3 subheading">利润中心*</v-subheader>
@@ -154,156 +155,178 @@
 </template>
 
 <script>
-  import {
-    getInitData
-  } from '@/api/prod'
-  import {
-    getProdData
-  } from '@/api/prod'
-  export default {
-    props: ['listValue'],
-    data: () => ({
-      title: null,
-      prodtypeData: '',
-      proddescData: '',
-      busimodelData: '',
-      prodclassData: '',
-      prodprepiceData: '',
-      prodmutiData: '',
-      prodstatusData: '',
-      baseprodData: '',
-      accttypeData: '',
-      acctsontypeData: '',
-      acctstructData: '',
-      virtualflagData: '',
-      acctintflagData: '',
-      amtflagData: '',
-      profitcenterData: '',
-      effectdateData: '',
-      failuredateData: '',
-      valid: true,
-      date: null,
-      startDateMenu: false,
-      startDate: '',
-      startTimeMenu: false,
-      startTime: null,
-      endDateMenu: false,
-      endDate: '',
-      endTimeMenu: false,
-      endTime: null,
-      modal: false,
-      listValue: '',
-      busimodel: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      prodclass: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      acctstruct: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      virtualflag: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      acctintflag: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      amtflag: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      baseprod: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      prodstatus: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      prodmuti: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      prodprepice: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      acctsontype: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      accttype: [{
-        prodCode: '',
-        prodDesc: ''
-      }],
-      profitcenter: [{
-        prodCode: '',
-        prodDesc: ''
-      }]
-    }),
-    computed: {
-      progress() {
-        return Math.min(100, this.value.length * 10)
-      },
-    },
-    watch: {
-        listValue(val, oldval) {
-            if (val !== oldval) {
-                this.selectByProd(val)
-            }
-        }
-    },
-    mounted() {
-      this.initRefDate()
-    },
-    methods: {
-      selectByProd() {
-        getProdData(this.listValue).then(response => {
-          this.prodtypeData = this.listValue
-          this.proddescData = response.data.prodFrom.proddesc
-          this.busimodelData = response.data.prodFrom.busimodel
-          this.prodclassData = response.data.prodFrom.prodclass
-          this.prodprepiceData = response.data.prodFrom.prodprepice
-          this.prodmutiData = response.data.prodFrom.prodmuti
-          this.prodstatusData = response.data.prodFrom.prodstatus
-          this.baseprodData = response.data.prodFrom.baseprod
-          this.accttypeData = response.data.prodFrom.accttype
-          this.acctsontypeData = response.data.prodFrom.acctsontype
-          this.acctstructData = response.data.prodFrom.acctstruct
-          this.virtualflagData = response.data.prodFrom.virtualflag
-          this.acctintflagData = response.data.prodFrom.acctintflag
-          this.amtflagData = response.data.prodFrom.amtflag
-          this.profitcenterData = response.data.prodFrom.profitcenter
-          this.startDate = response.data.prodFrom.effectdate
-          this.endDate = response.data.prodFrom.failuredate
-        })
-      },
-      initRefDate() {
-        getInitData().then(response => {
-          console.log(response)
-          this.busimodel = response.data.paraDataVl.busimodel
-          this.virtualflag = response.data.paraDataVl.virtualflag
-          this.acctintflag = response.data.paraDataVl.acctintflag
-          this.amtflag = response.data.paraDataVl.amtflag
-          this.prodmuti = response.data.paraDataVl.prodmuti
-          this.prodprepice = response.data.paraDataVl.prodprepice
-          this.accttype = response.data.paraDataVl.accttype
-          this.profitcenter = response.data.paraDataRf.profitcenter
-          this.acctsontype = response.data.paraDataRf.acctsontype
-          this.baseprod = response.data.paraDataRf.baseprod
-          this.acctstruct = response.data.paraDataRf.acctstruct
-          this.prodstatus = response.data.paraDataVl.prodstatus
-          this.prodclass = response.data.paraDataRf.prodclass
-        })
-      },
-      closeDialog() {
-        this.$parent.isActive = false;
+import { getInitData } from "@/api/prod";
+import { getProdData } from "@/api/prod";
+export default {
+  props: ["listValue"],
+  data: () => ({
+    title: null,
+    prodtypeData: "",
+    proddescData: "",
+    busimodelData: "",
+    prodclassData: "",
+    prodprepiceData: "",
+    prodmutiData: "",
+    prodstatusData: "",
+    baseprodData: "",
+    accttypeData: "",
+    acctsontypeData: "",
+    acctstructData: "",
+    virtualflagData: "",
+    acctintflagData: "",
+    amtflagData: "",
+    profitcenterData: "",
+    effectdateData: "",
+    failuredateData: "",
+    valid: true,
+    date: null,
+    startDateMenu: false,
+    startDate: "",
+    startTimeMenu: false,
+    startTime: null,
+    endDateMenu: false,
+    endDate: "",
+    endTimeMenu: false,
+    endTime: null,
+    modal: false,
+    listValue: "",
+    busimodel: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    prodclass: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    acctstruct: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    virtualflag: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    acctintflag: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    amtflag: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    baseprod: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    prodstatus: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    prodmuti: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    prodprepice: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    acctsontype: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    accttype: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ],
+    profitcenter: [
+      {
+        prodCode: "",
+        prodDesc: ""
+      }
+    ]
+  }),
+  computed: {
+    progress() {
+      return Math.min(100, this.value.length * 10);
+    }
+  },
+  watch: {
+    listValue(val, oldval) {
+      if (val !== oldval) {
+        this.selectByProd(val);
       }
     }
-  };
+  },
+  mounted() {
+    this.initRefDate();
+  },
+  methods: {
+    selectByProd() {
+      getProdData(this.listValue).then(response => {
+        this.prodtypeData = this.listValue;
+        this.proddescData = response.data.prodFrom.proddesc;
+        this.busimodelData = response.data.prodFrom.busimodel;
+        this.prodclassData = response.data.prodFrom.prodclass;
+        this.prodprepiceData = response.data.prodFrom.prodprepice;
+        this.prodmutiData = response.data.prodFrom.prodmuti;
+        this.prodstatusData = response.data.prodFrom.prodstatus;
+        this.baseprodData = response.data.prodFrom.baseprod;
+        this.accttypeData = response.data.prodFrom.accttype;
+        this.acctsontypeData = response.data.prodFrom.acctsontype;
+        this.acctstructData = response.data.prodFrom.acctstruct;
+        this.virtualflagData = response.data.prodFrom.virtualflag;
+        this.acctintflagData = response.data.prodFrom.acctintflag;
+        this.amtflagData = response.data.prodFrom.amtflag;
+        this.profitcenterData = response.data.prodFrom.profitcenter;
+        this.startDate = response.data.prodFrom.effectdate;
+        this.endDate = response.data.prodFrom.failuredate;
+      });
+    },
+    initRefDate() {
+      getInitData().then(response => {
+        console.log(response);
+        this.busimodel = response.data.paraDataVl.busimodel;
+        this.virtualflag = response.data.paraDataVl.virtualflag;
+        this.acctintflag = response.data.paraDataVl.acctintflag;
+        this.amtflag = response.data.paraDataVl.amtflag;
+        this.prodmuti = response.data.paraDataVl.prodmuti;
+        this.prodprepice = response.data.paraDataVl.prodprepice;
+        this.accttype = response.data.paraDataVl.accttype;
+        this.profitcenter = response.data.paraDataRf.profitcenter;
+        this.acctsontype = response.data.paraDataRf.acctsontype;
+        this.baseprod = response.data.paraDataRf.baseprod;
+        this.acctstruct = response.data.paraDataRf.acctstruct;
+        this.prodstatus = response.data.paraDataVl.prodstatus;
+        this.prodclass = response.data.paraDataRf.prodclass;
+      });
+    },
+    closeDialog() {
+      this.$parent.isActive = false;
+    }
+  }
+};
 </script>
