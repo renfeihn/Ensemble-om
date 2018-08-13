fcc033130c9844c4dc0b5fad816febec6f9b516d
chengliang <chengliang@dcits.com>
2018-08-13 15:47:14 +0800

    样式调整

--- src/views/userFlow/indexFlow/indexFlow2.vue
+++ src/views/userFlow/indexFlow/indexFlow2.vue
@@ -6,31 +6,38 @@
           <img src="/static/user/MyUser.png" class="imgUserIndexFlow">
         </v-flex>
         <v-flex lg7 sm7>
-          <span class="userSpan1 text-xs-center">admin,你好,欢迎进入</span>
-          <br/>
-          <span class="userSpan2 text-xs-center">XXX银行产品工厂 - 系统管理员</span>
+          <v-layout>
+            <v-flex lg7 sm7 pb-3>
+              <span class="headline text-xs-center">admin,你好,欢迎进入</span>
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-flex lg7 sm7>
+              <span class="title text-xs-center">XXX银行产品工厂 - 系统管理员</span>
+            </v-flex>
+          </v-layout>
         </v-flex>
         <v-flex lg3 sm3 offset-xs0 offset-lg2>
           <v-layout row wrap>
             <v-flex d-flex>
               <div>
-                <span>交易数</span>
-                <br/>
-                <span class="userP1">210</span>
+                <span class="title">交易数</span>
+                <br/><br/>
+                <span class="title pl-2">210</span>
               </div>
             </v-flex>
             <v-flex d-flex>
               <div>
-                <span>我的发布</span>
-                <br/>
-                <span class="userP1">45</span>
+                <span class="title">我的发布</span>
+                <br/><br/>
+                <span class="title pl-2">45</span>
               </div>
             </v-flex>
             <v-flex d-flex>
               <div>
-                <span>我的收藏</span>
-                <br/>
-                <span class="userP1">15</span>
+                <span class="title">我的收藏</span>
+                <br/><br/>
+                <span class="title pl-2">15</span>
               </div>
             </v-flex>
           </v-layout>
@@ -38,7 +45,7 @@
       </v-layout>
     </v-card>
     <v-layout row>
-      <v-flex xs9 sm9 class="userCard1" px-2>
+      <v-flex xs9 sm9 pt-2 pl-2>
         <v-card>
           <v-toolbar color="indigo lighten-2" dark scroll-off-screen scroll-target="#scrolling-techniques" flat>
             <v-toolbar-side-icon></v-toolbar-side-icon>
@@ -53,7 +60,7 @@
           </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs3 sm3 class="userCard1" px-2>
+      <v-flex xs3 sm3 pt-2 px-2>
         <v-card>
           <v-toolbar color="indigo lighten-2" dark scroll-off-screen scroll-target="#scrolling-techniques" flat>
             <v-toolbar-side-icon></v-toolbar-side-icon>
@@ -87,90 +94,72 @@
   </div>
 </template>
 <script>
-  import userWorkTags from '@/views/userFlow/userWork/userWorkTags'
-  export default {
-      components: {
-          userWorkTags
-      },
-      data() {
-          return {
-              items: [{
-                  icon: true,
-                  title: '对私存款产品',
-                  value: 'rbPrivate',
-                  avatar: '/static/prod/prod2.jpg'
-              },
-                  {
-                      icon: true,
-                      title: '对公贷款产品',
-                      value: 'clPublic',
-                      avatar: '/static/prod/prod2.jpg'
-                  },
-                  {
-                      icon: true,
-                      title: '参数管理入口',
-                      value: 'paramMag',
-                      avatar: '/static/prod/prod2.jpg'
-                  },
-                  {
-                      icon: true,
-                      title: '产品目录',
-                      value: 'prodList',
-                      avatar: '/static/prod/prod2.jpg'
-                  }
-              ]
-          }
-      },
-      methods: {
-          keyboardClick (item) {
-              var key = item.value
-              if(key === 'rbPrivate'){
-                  this.$router.push({ name: 'prod/rbPrivateProd', params: {'prodClassCmp': 'RB100', 'prodCodeCmp': '111001'}})
-              }
-              if(key === 'clPublic'){
-                  this.$router.push({ name: 'prod/clPublicProd', params: {'prodClassCmp': 'CL100', 'prodCodeCmp': '220001'}})
-              }
-//              if(key === 'paramMag'){
-//                  this.$router.push({ name: 'prod/rbPrivateProd'})
-//              }
-              if(key === 'prodList'){
-                  this.$router.push({ name: 'prodCmb'})
-              }
-
-          }
+import userWorkTags from "@/views/userFlow/userWork/userWorkTags";
+export default {
+  components: {
+    userWorkTags
+  },
+  data() {
+    return {
+      items: [
+        {
+          icon: true,
+          title: "对私存款产品",
+          value: "rbPrivate",
+          avatar: "/static/prod/prod2.jpg"
+        },
+        {
+          icon: true,
+          title: "对公贷款产品",
+          value: "clPublic",
+          avatar: "/static/prod/prod2.jpg"
+        },
+        {
+          icon: true,
+          title: "参数管理入口",
+          value: "paramMag",
+          avatar: "/static/prod/prod2.jpg"
+        },
+        {
+          icon: true,
+          title: "产品目录",
+          value: "prodList",
+          avatar: "/static/prod/prod2.jpg"
+        }
+      ]
+    };
+  },
+  methods: {
+    keyboardClick(item) {
+      var key = item.value;
+      if (key === "rbPrivate") {
+        this.$router.push({
+          name: "prod/rbPrivateProd",
+          params: { prodClassCmp: "RB100", prodCodeCmp: "111001" }
+        });
       }
+      if (key === "clPublic") {
+        this.$router.push({
+          name: "prod/clPublicProd",
+          params: { prodClassCmp: "CL100", prodCodeCmp: "220001" }
+        });
+      }
+      //              if(key === 'paramMag'){
+      //                  this.$router.push({ name: 'prod/rbPrivateProd'})
+      //              }
+      if (key === "prodList") {
+        this.$router.push({ name: "prodCmb" });
+      }
+    }
   }
+};
 </script>
 <style scoped>
-  .imgUserIndexFlow {
-    width: 100px;
-    display: inline-block;
-  }
-  .userCard1 {
-    padding: 20px;
-  }
-  .userSpan1 {
-    font-size: 26px;
-    display: inline-block;
-    padding-top: 10px;
-  }
-  .userSpan2 {
-    display: inline-block;
-    padding-top: 5px;
-  }
-  .userDiv1 {
-    float: left;
-    text-align: center;
-    margin-top: 10px;
-  }
-  .userDiv3 {
-    padding: 10px;
-  }
-  .indexFlowTitle {
-    padding: 10px;
-  }
-  .userP1 {
-    font-size: 25px;
-    display: inline-block;
-  }
+.imgUserIndexFlow {
+  width: 100px;
+  display: inline-block;
+}
+.indexFlowTitle {
+  padding: 10px;
+}
 </style>
