18cd16b242d75c6394526f2a42fb726d75c720f8
jiajt <jiajt@dcits.com>
2018-08-20 15:54:20 +0800

    修改变量名称

--- src/server/pkList.js
+++ src/server/pkList.js
@@ -2,10 +2,10 @@
 const fs = require("fs");
 // var Finder = require('fs-finder');
 const glob = require("glob");
-var files = '../../data/server/dictionarys'
+var path = '../../data/server/dictionarys'
 
 export function getPkList (params) {
-    var files = glob.sync(files, {nodir: true})
+    var files = glob.sync(path, {nodir: true})
     files.forEach(function (filePath) {
         const statFile = fs.statSync(filePath);
         // var paths = Finder.in(files).find();
