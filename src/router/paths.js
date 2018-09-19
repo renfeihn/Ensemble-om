cac8e53be8a2fdefc78f0f87589d465eaf98eb87
jiajt <jiajt@dcits.com>
2018-09-19 15:24:24 +0800

    合并存款活期产品修改

--- src/router/paths.js
+++ src/router/paths.js
@@ -819,7 +819,7 @@ export default [
     path: '/prod/rbPrivateProds',
         meta: {
     breadcrumb: true,
-        title: '活期存款组'
+        title: '活期产品组'
 },
     name: 'prod/rbPrivateProds',
         component: () =>
