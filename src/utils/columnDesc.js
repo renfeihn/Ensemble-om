c266827395a47addc7a15e5c1654dbf7ee0a1037
huisn <huisn@dcits.com>
2018-09-27 17:46:56 +0800

    列描述数据维护

--- src/utils/columnDesc.js
+++ src/utils/columnDesc.js
@@ -24,7 +24,32 @@ const column= {
     '10': '10-实时生效',
     '11': '11-日终处理',
     'Y': 'Y-持续扣款',
-    'N': 'N-非持续扣款'
+    'N': 'N-非持续扣款',
+    'EVENT_TYPE': '事件类型',
+    'INT_CLASS': '利息分类',
+    'INT_TYPE': '利率代码',
+    'INT_TYPE_DESC': '利率代码详细',
+    'INT_CALC_BAL': '计息方式',
+    'INT_AMT_ID': '利息计算金额编码',
+    'RATE_AMT_ID': '利率计算金额编码',
+    'MONTH_BASIS': '月基准',
+    'ACCOUNTING_STATUS': '核算状态',
+    'BUSINESS_UNIT': '套账',
+    'GL_CODE_L': '负债科目代码',
+    'GL_CODE_INT_E': '利息支出科目代码',
+    'GL_CODE_INT_PAY': '应付利息科目代码',
+    'FEE_TYPE': '批量收费类型',
+    'CHARGE_PERIOD_FREQ': '收费频率',
+    'CHARGE_DAY': '收费日期',
+    'CHARGE_DEAL_METHOD': '收费处理方式',
+    'CON_DEDUCT_FLAG': '持续扣款标识',
+    'CON_DEDUCT_TIMES': '持续扣款次数',
+    'ACCT_STATUS': '账户状态',
+    'TRANSFER_TYPE': '转移类型',
+    'TRANSFER_PERIOD': '转移周期',
+    'RESTRAINT_AMT': '限制金额',
+    'TRANSFER_CONDITION': '转移条件',
+    'TRAN_AFTER_FLAG': '转移后是否结息'
 }
 
 export function getColumnDesc (data) {
