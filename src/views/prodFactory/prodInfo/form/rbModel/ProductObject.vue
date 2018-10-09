eea8011c14420daf35fd79b7203db342ca81b027
huisn <huisn@dcits.com>
2018-10-09 13:23:05 +0800

    适用范围页面多选功能改进

--- src/views/prodFactory/prodInfo/form/rbModel/ProductObject.vue
+++ src/views/prodFactory/prodInfo/form/rbModel/ProductObject.vue
@@ -9,44 +9,42 @@
                                 <v-subheader class="primary--text subheading">客户类型*</v-subheader>
                             </v-flex>
                             <v-flex md4 lg4>
-                                <v-select class="primary--text mx-2" :items="clientType" v-bind:value="productObject.clientType | getValues" label="客户类型" item-text="value" item-value="key" multiple single-line hide-details></v-select>
+                                <dc-multiselect v-model="productObject.clientType" :options="clientType"></dc-multiselect>
                             </v-flex>
                             <v-flex xs12 md2 lg2>
                                 <v-subheader class="primary--text subheading">境内外标识*</v-subheader>
                             </v-flex>
                             <v-flex md4 lg4>
-                                <v-select class="primary--text mx-2" :items="inlandOffshore" v-bind:value="productObject.inLandOffshore | getValues" label="境内外标识" item-text="value" item-value="key" multiple single-line hide-details></v-select>
+                                <dc-multiselect v-model="productObject.inlandOffshore" :options="inlandOffshore"></dc-multiselect>
                             </v-flex>
                             <v-flex xs12 md2 lg2>
                                 <v-subheader class="primary--text subheading">客户子类型*</v-subheader>
                             </v-flex>
                             <v-flex md4 lg4>
-                                <v-select class="primary--text mx-2" :items="clientSubType" v-bind:value="productObject.clientSubType | getValues" label="客户子类型" item-text="value" item-value="key" multiple single-line hide-details></v-select>
+                                <dc-multiselect v-model="productObject.clientSubType" :options="clientSubType"></dc-multiselect>
                             </v-flex>
                             <v-flex xs12 md2 lg2>
                                 <v-subheader class="primary--text subheading">客户等级*</v-subheader>
                             </v-flex>
                             <v-flex md4 lg4>
-                                <v-select class="primary--text mx-2" :items="clientLevel" v-bind:value="productObject.clientInd | getValues" label="客户等级" item-text="value" item-value="key" multiple single-line hide-details ></v-select>
+                                <dc-multiselect v-model="productObject.clientInd" :options="clientInd"></dc-multiselect>
                             </v-flex>
                             <v-flex xs12 md2 lg2>
                                 <v-subheader class="primary--text subheading">年龄区间*</v-subheader>
                             </v-flex>
                             <v-flex md4 lg4>
-                                <v-select class="primary--text mx-2" :items="ageSection" v-bind:value="productObject.ageSection | getValues" label="年龄区间" item-text="value" item-value="key" multiple single-line hide-details ></v-select>
+                                <dc-multiselect v-model="productObject.ageSection" :options="ageSection"></dc-multiselect>
                             </v-flex>
                             <v-flex xs12 md2 lg2>
                                 <v-subheader class="primary--text subheading">职业*</v-subheader>
                             </v-flex>
                             <v-flex md4 lg4>
-                                <v-select class="primary--text mx-2" :items="profession" v-bind:value="productObject.occupation | getValues" label="职业" item-text="value" item-value="key" multiple single-line hide-details ></v-select>
-                            </v-flex>
+                                <dc-multiselect v-model="productObject.occupation" :options="occupation"></dc-multiselect>                            </v-flex>
                             <v-flex xs12 md2 lg2>
                                 <v-subheader class="primary--text subheading">渠道选择*</v-subheader>
                             </v-flex>
                             <v-flex md4 lg4>
-                                <v-select class="primary--text mx-2" :items="channelChoose" v-bind:value="productObject.channelChoose | getValues" label="渠道选择" item-text="value" item-value="key" multiple single-line hide-details></v-select>
-                            </v-flex>
+                                <dc-multiselect v-model="productObject.channelChoose" :options="channelChoose"></dc-multiselect>                               </v-flex>
                             <v-flex xs12 md2 lg2>
                                 <v-subheader class="primary--text subheading">适用区域*</v-subheader>
                             </v-flex>
@@ -57,8 +55,7 @@
                                 <v-subheader class="primary--text subheading">区域选择*</v-subheader>
                             </v-flex>
                             <v-flex md4 lg4>
-                                <v-select class="primary--text mx-2" :items="areaChoose" v-bind:value="productObject.areaChoose | getValues" label="区域选择" item-text="value" item-value="key" multiple single-line hide-details></v-select>
-                            </v-flex>
+                                <dc-multiselect v-model="productObject.areaChoose" :options="areaChoose"></dc-multiselect>                               </v-flex>
                             <v-flex xs12 md2 lg2>
                                 <v-subheader class="primary--text subheading">适用机构*</v-subheader>
                             </v-flex>
@@ -70,8 +67,7 @@
                                 <v-subheader class="primary--text subheading">机构选择*</v-subheader>
                             </v-flex>
                             <v-flex md4 lg4>
-
-                                <v-select class="primary--text mx-2" :items="branchChoice" v-bind:value="productObject.prodBranch | getValues" label="机构选择" item-text="value" item-value="key" multiple single-line hide-details></v-select>
+                                <dc-multiselect v-model="productObject.branchChoice" :options="branchChoice"></dc-multiselect>
                             </v-flex>
                         </v-layout>
                     </v-container>
@@ -83,9 +79,11 @@
 
 <script>
     import DcSwitch from "@/components/widgets/DcSwitch";
+    import DcMultiselect from '@/components/widgets/DcMultiselect'
     import { getInitData } from "@/mock/init";
     import { getProdData } from "@/api/prod";
     export default {
+        components: { DcSwitch, DcMultiselect },
         filters: {
             getValues: function (key) {
                 let value=[];
@@ -95,16 +93,55 @@
                 return value
             }
         },
-        components: {DcSwitch},
         props: ["prodData"],
         data: () => ({
             title: null,
             refData: getInitData,
+            clientType: [{
+                key: '',
+                value: ''
+            }],
+            inlandOffshore: [{
+                key: '',
+                value: ''
+            }],
+            clientSubType: [{
+                key: '',
+                value: ''
+            }],
+            clientInd: [{
+                key: '',
+                value: ''
+            }],
+            ageSection: [{
+                key: '',
+                value: ''
+            }],
+            occupation: [{
+                key: '',
+                value: ''
+            }],
+            channelChoose: [{
+                key: '',
+                value: ''
+            }],
+            applyBranch: '',
+            branchChoice: [{
+                key: '',
+                value: ''
+            }],
+            applyArea: '',
+            areaChoose: [{
+                key: '',
+                value: ''
+            }],
+            fixedCall: '',
+            prodBranch: '',
             productObject: {
                 clientType: '',
-                inLandOffshore: '',
+                inlandOffshore: '',
                 clientSubType: '',
-                clientLevel: '',
+                clientInd: '',
                 ageSection: '',
                 occupation: '',
                 channelChoose: '',
@@ -113,7 +150,6 @@
                 applyArea: '',
                 areaChoose: '',
                 fixedCall: '',
-                clientInd: '',
                 prodBranch: ''
             }
         }),
@@ -137,14 +173,19 @@
             selectByProd(val) {
                 this.productObject = {}
                 this.productObject.clientType = val.prodDefines.CLIENT_TYPE.attrValue
+<<<<<<< Updated upstream
                 this.productObject.inLandOffshore = val.prodDefines.INLAND_OFFSHORE.attrValue
                 this.productObject.applyBranch = val.prodDefines.APPLY_BRANCH.attrValue//适用机构
+=======
+                this.productObject.inlandOffshore = val.prodDefines.INLAND_OFFSHORE.attrValue
+                this.productObject.prodBranch = val.prodDefines.APPLY_BRANCH.attrValue//适用机构
+>>>>>>> Stashed changes
                 this.productObject.clientInd = val.prodDefines.CLIENT_IND.attrValue
                 this.productObject.clientSubType = val.prodDefines.CLIENT_GENERAL_TYPE.attrValue//客户子类型
                 this.productObject.channelChoose = ""//渠道选择
                 this.productObject.ageSection = val.prodDefines.AGE_PART.attrValue//年龄区间
                 this.productObject.occupation = val.prodDefines.PROFESSION.attrValue//职业
-                this.productObject.prodBranch = val.prodDefines.PROD_BRANCH.attrValue//机构选择
+                this.productObject.branchChoice = val.prodDefines.PROD_BRANCH.attrValue//机构选择
                 this.productObject.applyArea = val.prodDefines.ADUPTER_AREA.attrValue//适用区域
                 this.productObject.areaChoose = val.prodDefines.AREA_TYPE.attrValue//区域选择
             },
@@ -152,9 +193,9 @@
                 this.clientType = this.refData[2].paraDataRb.clientType;
                 this.inlandOffshore = this.refData[2].paraDataRb.inlandOffshore;
                 this.clientSubType = this.refData[2].paraDataRb.clientSubType;
-                this.clientLevel = this.refData[2].paraDataRb.clientLevel;
+                this.clientInd = this.refData[2].paraDataRb.clientLevel;
                 this.ageSection = this.refData[2].paraDataRb.ageSection;
-                this.profession = this.refData[2].paraDataRb.occupation;
+                this.occupation = this.refData[2].paraDataRb.occupation;
                 this.channelChoose = this.refData[2].paraDataRb.channelChoose;
                 this.prodBranch = this.refData[2].paraDataRb.applyBranch;
                 this.branchChoice = this.refData[2].paraDataRb.branchChoice;
