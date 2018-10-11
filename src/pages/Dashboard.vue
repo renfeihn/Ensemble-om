91668e6416d3ff7344d8e65084e29eb74af88965
chengliang <chengliang@dcits.com>
2018-10-11 10:21:03 +0800

    首页产品统计缩写

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -6,6 +6,7 @@
         <v-flex lg3 sm6 xs12>
           <mini-statistic
                   icon="fa fa-facebook"
+                  mark="RB"
                   title="存款产品"
                   sub-title="210"
                   color="indigo"
@@ -15,6 +16,7 @@
         <v-flex lg3 sm6 xs12>
           <mini-statistic
                   icon="fa fa-google"
+                  mark="CL"
                   title="贷款产品"
                   sub-title="187"
                   color="red"
@@ -24,6 +26,7 @@
         <v-flex lg3 sm6 xs12>
           <mini-statistic
                   icon="fa fa-twitter"
+                  mark="GL"
                   title="内部帐"
                   sub-title="120"
                   color="light-blue"
@@ -33,6 +36,7 @@
         <v-flex lg3 sm6 xs12>
           <mini-statistic
                   icon="fa fa-instagram"
+                  mark="SF"
                   title="特色理财"
                   sub-title="20"
                   color="purple"
@@ -99,145 +103,144 @@
 </template>
 
 <script>
-    import API from '@/api';
-    import EChart from '@/components/chart/echart';
-    import MiniStatistic from '@/components/widgets/statistic/MiniStatistic';
-    import PostListCard from '@/components/widgets/card/PostListCard';
-    import ProfileCard from '@/components/widgets/card/ProfileCard';
-    import PostSingleCard from '@/components/widgets/card/PostSingleCard';
-    import WeatherCard from '@/components/widgets/card/WeatherCard';
-    import PlainTable from '@/components/widgets/list/PlainTable';
-    import PlainTableOrder from '@/components/widgets/list/PlainTableOrder';
-    import VWidget from '@/components/VWidget';
-    import Material from 'vuetify/es5/util/colors';
-    import VCircle from '@/components/circle/VCircle';
-    import BoxChart from '@/components/widgets/chart/BoxChart';
-    import ChatWindow from '@/components/chat/ChatWindow';
-    import CircleStatistic from '@/components/widgets/statistic/CircleStatistic';
-    import LinearStatistic from '@/components/widgets/statistic/LinearStatistic';
-    export default {
-        components: {
-            VWidget,
-            MiniStatistic,
-            ChatWindow,
-            VCircle,
-            WeatherCard,
-            PostSingleCard,
-            PostListCard,
-            ProfileCard,
-            EChart,
-            BoxChart,
-            CircleStatistic,
-            LinearStatistic,
-            PlainTable,
-            PlainTableOrder
+import API from "@/api";
+import EChart from "@/components/chart/echart";
+import MiniStatistic from "@/components/widgets/statistic/MiniStatistic";
+import PostListCard from "@/components/widgets/card/PostListCard";
+import ProfileCard from "@/components/widgets/card/ProfileCard";
+import PostSingleCard from "@/components/widgets/card/PostSingleCard";
+import WeatherCard from "@/components/widgets/card/WeatherCard";
+import PlainTable from "@/components/widgets/list/PlainTable";
+import PlainTableOrder from "@/components/widgets/list/PlainTableOrder";
+import VWidget from "@/components/VWidget";
+import Material from "vuetify/es5/util/colors";
+import VCircle from "@/components/circle/VCircle";
+import BoxChart from "@/components/widgets/chart/BoxChart";
+import ChatWindow from "@/components/chat/ChatWindow";
+import CircleStatistic from "@/components/widgets/statistic/CircleStatistic";
+import LinearStatistic from "@/components/widgets/statistic/LinearStatistic";
+export default {
+  components: {
+    VWidget,
+    MiniStatistic,
+    ChatWindow,
+    VCircle,
+    WeatherCard,
+    PostSingleCard,
+    PostListCard,
+    ProfileCard,
+    EChart,
+    BoxChart,
+    CircleStatistic,
+    LinearStatistic,
+    PlainTable,
+    PlainTableOrder
+  },
+  data: () => ({
+    color: Material,
+    selectedTab: "tab-1",
+    linearTrending: [
+      {
+        subheading: "Sales",
+        headline: "2,55",
+        caption: "increase",
+        percent: 15,
+        icon: {
+          label: "trending_up",
+          color: "success"
         },
-        data: () => ({
-            color: Material,
-            selectedTab: 'tab-1',
-            linearTrending: [
-                {
-                    subheading: 'Sales',
-                    headline: '2,55',
-                    caption: 'increase',
-                    percent: 15,
-                    icon: {
-                        label: 'trending_up',
-                        color: 'success'
-                    },
-                    linear: {
-                        value: 15,
-                        color: 'success'
-                    }
-                },
-                {
-                    subheading: 'Revenue',
-                    headline: '6,553',
-                    caption: 'increase',
-                    percent: 10,
-                    icon: {
-                        label: 'trending_down',
-                        color: 'error'
-                    },
-                    linear: {
-                        value: 15,
-                        color: 'error'
-                    }
-                },
-                {
-                    subheading: 'Orders',
-                    headline: '5,00',
-                    caption: 'increase',
-                    percent: 50,
-                    icon: {
-                        label: 'arrow_upward',
-                        color: 'info'
-                    },
-                    linear: {
-                        value: 50,
-                        color: 'info'
-                    }
-                }
-            ],
-            trending: [
-                {
-                    subheading: 'Email',
-                    headline: '15+',
-                    caption: 'email opens',
-                    percent: 15,
-                    icon: {
-                        label: 'email',
-                        color: 'info'
-                    },
-                    linear: {
-                        value: 15,
-                        color: 'info'
-                    }
-                },
-                {
-                    subheading: 'Tasks',
-                    headline: '90%',
-                    caption: 'tasks completed.',
-                    percent: 90,
-                    icon: {
-                        label: 'list',
-                        color: 'primary'
-                    },
-                    linear: {
-                        value: 90,
-                        color: 'success'
-                    }
-                },
-                {
-                    subheading: 'Issues',
-                    headline: '100%',
-                    caption: 'issues fixed.',
-                    percent: 100,
-                    icon: {
-                        label: 'bug_report',
-                        color: 'primary'
-                    },
-                    linear: {
-                        value: 100,
-                        color: 'error'
-                    }
-                },
-            ]
-        }),
-        computed: {
-            activity () {
-                return API.getActivity();
-            },
-            posts () {
-                return API.getPost(3);
-            },
-            siteTrafficData () {
-                return API.getMonthVisit;
-            },
-            locationData () {
-                return API.getLocation;
-            }
+        linear: {
+          value: 15,
+          color: "success"
+        }
+      },
+      {
+        subheading: "Revenue",
+        headline: "6,553",
+        caption: "increase",
+        percent: 10,
+        icon: {
+          label: "trending_down",
+          color: "error"
         },
-
-    };
+        linear: {
+          value: 15,
+          color: "error"
+        }
+      },
+      {
+        subheading: "Orders",
+        headline: "5,00",
+        caption: "increase",
+        percent: 50,
+        icon: {
+          label: "arrow_upward",
+          color: "info"
+        },
+        linear: {
+          value: 50,
+          color: "info"
+        }
+      }
+    ],
+    trending: [
+      {
+        subheading: "Email",
+        headline: "15+",
+        caption: "email opens",
+        percent: 15,
+        icon: {
+          label: "email",
+          color: "info"
+        },
+        linear: {
+          value: 15,
+          color: "info"
+        }
+      },
+      {
+        subheading: "Tasks",
+        headline: "90%",
+        caption: "tasks completed.",
+        percent: 90,
+        icon: {
+          label: "list",
+          color: "primary"
+        },
+        linear: {
+          value: 90,
+          color: "success"
+        }
+      },
+      {
+        subheading: "Issues",
+        headline: "100%",
+        caption: "issues fixed.",
+        percent: 100,
+        icon: {
+          label: "bug_report",
+          color: "primary"
+        },
+        linear: {
+          value: 100,
+          color: "error"
+        }
+      }
+    ]
+  }),
+  computed: {
+    activity() {
+      return API.getActivity();
+    },
+    posts() {
+      return API.getPost(3);
+    },
+    siteTrafficData() {
+      return API.getMonthVisit;
+    },
+    locationData() {
+      return API.getLocation;
+    }
+  }
+};
 </script>
