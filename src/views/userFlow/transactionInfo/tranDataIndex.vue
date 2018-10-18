051d40f384d4684fb2ba102a4ec7c9b40908ce13
huisn <huisn@dcits.com>
2018-10-18 17:33:46 +0800

    交易详细信息页面调整

--- src/views/userFlow/transactionInfo/tranDataIndex.vue
+++ src/views/userFlow/transactionInfo/tranDataIndex.vue
@@ -1,15 +1,97 @@
 <template>
   <div>
-    <v-card >
-      <v-toolbar color="white" >
-        <v-icon large color="blue darken-2">chat</v-icon>
-        <v-toolbar-title>交易单号:{{code}}</v-toolbar-title>
-        <v-spacer></v-spacer>
-          <v-btn color="info" @click="print()">导出</v-btn>
-          <v-btn color="info">{{optValue}}</v-btn>
-      </v-toolbar>
-      <tran-check-flow-info v-if="optKey==3" ></tran-check-flow-info>
-      <tran-release-flow-info v-if="optKey==4"></tran-release-flow-info>
+    <v-layout row wrap>
+    <v-flex xs12 md4 lg4>
+      <v-card>
+        <v-toolbar color="white">
+          <v-icon large color="blue darken-2">info</v-icon>
+          <v-toolbar-title>产品基本信息{{info}}</v-toolbar-title>
+        </v-toolbar>
+
+        <v-card-text>
+          <v-layout wrap>
+            <v-flex xs12 md2 lg4>
+              <v-subheader class="primary--text subheading">组合产品*</v-subheader>
+            </v-flex>
+            <v-flex md4 lg4>
+              <dc-text-field v-model="prodGroup" item-text="value" item-value="key"></dc-text-field>
+            </v-flex>
+          </v-layout>
+
+          <v-layout wrap>
+          <v-flex xs12 md2 lg4>
+            <v-subheader class="primary--text subheading">产品分类*</v-subheader>
+          </v-flex>
+          <v-flex md4 lg4>
+            <dc-text-field v-model="prodClass"></dc-text-field>
+          </v-flex>
+          </v-layout>
+
+          <v-layout wrap>
+          <v-flex xs12 md2 lg4>
+            <v-subheader class="primary--text subheading">产品描述*</v-subheader>
+          </v-flex>
+          <v-flex md4 lg4>
+            <dc-text-field v-model="prodDesc"></dc-text-field>
+          </v-flex>
+          </v-layout>
+
+          <v-layout wrap>
+          <v-flex xs12 md2 lg4>
+            <v-subheader class="primary--text subheading">产品状态*</v-subheader>
+          </v-flex>
+          <v-flex md4 lg4>
+            <dc-text-field v-model="status"></dc-text-field>
+          </v-flex>
+          </v-layout>
+
+          <v-layout wrap>
+          <v-flex xs12 md2 lg4>
+            <v-subheader class="primary--text subheading">基础产品*</v-subheader>
+          </v-flex>
+          <v-flex md4 lg4>
+            <dc-text-field v-model="baseProdType"></dc-text-field>
+          </v-flex>
+          </v-layout>
+
+          <v-layout wrap>
+            <v-flex xs12 md2 lg4>
+              <v-subheader class="primary--text subheading">产品范围*</v-subheader>
+            </v-flex>
+            <v-flex md4 lg4>
+              <dc-text-field v-model="prodRange"></dc-text-field>
+            </v-flex>
+          </v-layout>
+
+          <v-layout wrap>
+            <v-flex xs12 md2 lg4>
+              <v-subheader class="primary--text subheading">产品类型*</v-subheader>
+            </v-flex>
+            <v-flex md4 lg4>
+              <dc-text-field v-model="prodType"></dc-text-field>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+
+    <v-flex md8 lg8>
+      <v-card>
+        <v-toolbar color="white" >
+          <v-icon large color="blue darken-2">chat</v-icon>
+          <v-toolbar-title>交易单号:{{code}}</v-toolbar-title>
+          <v-spacer></v-spacer>
+            <v-btn color="info" @click="print()">导出</v-btn>
+            <v-btn color="info">{{optValue}}</v-btn>
+        </v-toolbar>
+        <tran-check-flow-info v-if="optKey==3"></tran-check-flow-info>
+        <tran-release-flow-info v-if="optKey==4"></tran-release-flow-info>
+      </v-card>
+    </v-flex>
+    </v-layout>
+
+
+    <v-card md20 lg20>
       <v-card-text>
         <v-tabs fixed-tabs>
           <v-tab v-for="n in diffList"
@@ -37,7 +119,6 @@
       </v-card-text>
     </v-card>
 
-
   </div>
 </template>
 <script>
@@ -47,14 +128,18 @@
   import tranReleaseFlowInfo from './tranReleaseFlowInfo'
   import { getDiffList } from "@/api/url/prodInfo";
   import {getColumnDesc_} from '@/utils/columnDesc'
+  import DcTextField from '@/components/widgets/DcTextField'
+  import { getProdData } from "@/api/prod";
   export default {
         components: {
             prodDiff,
             baseTable,
             tranCheckFlowInfo,
             tranReleaseFlowInfo,
-            getColumnDesc_
+            getColumnDesc_,
+            DcTextField
         },
+      props: ["prodData"],
       data (){
           return {
               code: '',
@@ -68,7 +153,31 @@
               prodEventCret: {},
               prodEventCycle: {},
               prodEventDebt: {},
-              diffList: ["产品属性","开户定义","销户定义","存入定义","支取定义","利息信息","收费定义"]
+              diffList: ["产品属性","开户定义","销户定义","存入定义","支取定义","利息信息","收费定义"],
+              prodGroup: [{
+                  key: 'Y',
+                  value: 'Y-是'
+              }],
+              prodClass: [{
+                  key: 'RB100',
+                  value: 'RB100-A'
+              }],
+              prodRange: [{
+                  key: '',
+                  value: ''
+              }],
+              status: [{
+                  key: '',
+                  value: ''
+              }],
+              baseProdType: [{
+                  key: '',
+                  value: ''
+              }],
+              prodType: [{
+                  key: '',
+                  value: ''
+              }]
           }
       },
 
@@ -99,7 +208,14 @@
                     this.assembleEvent();
                     //将收费定义的差异组装
                     this.assembleProdCharge();
-
+                    this.prodGroup = this.prodData.MbProdType.prodGroup
+                    this.prodClass = this.prodData.MbProdType.prodClass
+                    this.prodDesc = this.prodData.MbProdType.prodDesc
+                    this.prodType = this.prodData.MbProdType.prodGroup
+                    this.status = this.prodData.MbProdType.status
+                    this.baseProdType = this.prodData.MbProdType.baseProdType
+                    this.prodRange = this.prodData.MbProdType.prodRange
+                    this.prodType = this.prodData.MbProdType.prodType
                 });
             },
             assembleProdDefine() {
