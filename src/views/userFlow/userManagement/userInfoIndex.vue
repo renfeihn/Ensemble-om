a687ac80b57c34493944d3a36a810a730d845b87
jiajt <jiajt@dcits.com>
2018-11-26 19:58:00 +0800

    个人信息维护修改

--- src/views/userFlow/userManagement/userInfoIndex.vue
+++ src/views/userFlow/userManagement/userInfoIndex.vue
@@ -1,21 +1,28 @@
 <template>
-    <v-card class="elevation-2">
+    <div class="app-container pt-3">
+    <v-card>
         <v-layout row wrap>
             <v-flex md2 lg2>
                 <v-navigation-drawer v-model="drawer" permanent absolute>
-                    <v-toolbar flat class="transparent">
-                        <v-list class="pa-0">
-                            <v-list-tile avatar>
-                                <v-list-tile-avatar>
-                                    <img src="/static/avatar/timg.jpg">
-                                </v-list-tile-avatar>
-                                <v-list-tile-content>
-                                    <v-list-tile-title>admin</v-list-tile-title>
-                                </v-list-tile-content>
-                            </v-list-tile>
-                        </v-list>
-                    </v-toolbar>
-
+                    <v-card>
+                        <v-card-media src="/static/nature/tree2.png" class="class1">
+                            <v-card-title class="pb-0">
+                                <v-toolbar flat class="transparent">
+                                    <v-list class="pa-0" style="margin-left: -40px">
+                                        <v-list-tile avatar>
+                                            <v-list-tile-avatar>
+                                                <img src="/static/avatar/timg.jpg">
+                                            </v-list-tile-avatar>
+                                            <v-list-tile-content>
+                                                <v-list-tile-title class="class2">Admin</v-list-tile-title>
+                                            </v-list-tile-content>
+                                        </v-list-tile>
+                                    </v-list>
+                                </v-toolbar>
+                                <h4 class="title class3">系统管理员</h4>
+                            </v-card-title>
+                        </v-card-media>
+                    </v-card>
                     <v-list class="pt-0" dense>
                         <v-divider></v-divider>
                         <v-list-tile v-for="item in items" :key="item.title" @click="actionTag(item)" :class="item.class">
@@ -23,18 +30,19 @@
                                 <v-icon>{{ item.icon }}</v-icon>
                             </v-list-tile-action>
                             <v-list-tile-content>
-                                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                                <v-list-tile-title style="font-size: medium">{{ item.title }}</v-list-tile-title>
                             </v-list-tile-content>
                         </v-list-tile>
                         </v-list>
                 </v-navigation-drawer>
             </v-flex>
-            <v-flex md10 lg10>
+            <v-flex md10 lg10 class="pl-5">
                 <user-info v-if="action=='userInfo'"></user-info>
                 <user-password v-else></user-password>
             </v-flex>
         </v-layout>
     </v-card>
+    </div>
 </template>
 <script>
     import UserInfo from './userInfo'
@@ -49,8 +57,8 @@
             return {
                 drawer: true,
                 items: [
-                    {title: '个人信息管理', name: 'userInfo', icon: 'dashboard', class: ''},
-                    {title: '密码管理', name: 'userPassword', icon: 'question_answer', class: ''}
+                    {title: '个人信息管理', name: 'userInfo', icon: 'person', class: ''},
+                    {title: '密码管理', name: 'userPassword', icon: 'lock', class: ''}
                 ],
                 right: null,
                 action: 'userInfo'
@@ -72,5 +80,14 @@
 <style>
     .selected {
         background-color: #e8e8e8;
+    }
+    .class1 {
+        width: 100%;height: 220px
+    }
+    .class2 {
+        color: white;font-size: x-large
+    }
+    .class3 {
+        font-size: large; color: white; margin-left: 5px
     }
 </style>
--- src/views/userFlow/userManagement/userPassword.vue
+++ src/views/userFlow/userManagement/userPassword.vue
@@ -1,43 +1,20 @@
 <template>
-    <form>
-        <v-text-field
-                v-model="userId"
-                label="用户名"
-                disabled= true
-                required
-        ></v-text-field>
-        <v-text-field
-                v-model="password"
-                :rules="passwordErrors"
-                label="原密码"
-                required
-        ></v-text-field>
-        <v-text-field
-                v-model="newPassword"
-                label="新密码"
-                :rules="passwordErrors"
-                required
-        ></v-text-field>
-        <v-text-field
-                v-model="newPasswordEnd"
-                label="新密码"
-                :rules="[v => v==newPassword||'与上次输入新密码不一致']"
-                required
-        ></v-text-field>
-        <v-checkbox
-                v-validate="'required'"
-                v-model="checkbox"
-                :error-messages="errors.collect('checkbox')"
-                value="1"
-                label="修改密码将重新跳到登录页"
-                data-vv-name="checkbox"
-                type="checkbox"
-                required
-        ></v-checkbox>
+    <v-layout row wrap>
+        <h4 style="margin-left: 6%;color: #00b0ff;font-size: x-large;margin-top: 4%">密码管理</h4>
+        <form class="class2">
+            <v-text-field v-model="userId" label="用户ID" disabled= true required append-icon="person"></v-text-field>
+            <v-text-field v-model="password" :rules="passwordErrors" label="原密码" required append-icon="lock"></v-text-field>
+            <v-text-field v-model="newPassword" label="新密码" :rules="passwordErrors" required append-icon="lock"></v-text-field>
+            <v-text-field v-model="newPasswordEnd" label="再次输入新密码" :rules="[v => v==newPassword||'与上次输入新密码不一致']" required append-icon="info"></v-text-field>
+                <h5 style="color: orangered">
+                    <v-icon style="color: orangered">info</v-icon>
+                    修改成功后将重新登录
+                </h5>
+            <v-btn @click="submit" style="margin-left: 70%;width: 122px;color: white;background-color: #00b0ff" round>确   认</v-btn>
+            <v-btn @click="clear" style="margin-left: 3%; width: 122px;color: white;background-color: #00b0ff" round>取   消</v-btn>
+        </form>
+    </v-layout>
 
-        <v-btn @click="submit">确认</v-btn>
-        <v-btn @click="clear">取消</v-btn>
-    </form>
 </template>
 <script>
     import axios from 'axios'
@@ -82,3 +59,15 @@
         }
     }
 </script>
+
+<style>
+    .btn {
+        width: 122px;
+    }
+    .class2 {
+        width: 90%;margin-left: 6%;height: 500px;margin-top: 4%
+    }
+    .class3 {
+        font-size: large; color: white; margin-left: 5px
+    }
+</style>
