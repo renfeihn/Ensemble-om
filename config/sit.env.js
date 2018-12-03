0b8bbfd75d43143b5eb9b8be7d247fa6a6220607
jiajt <jiajt@dcits.com>
2018-12-03 17:31:47 +0800

    build打包文件提交

--- config/sit.env.js
+++ config/sit.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   MOCK_SWITCH: '"off"',
-  BASE_API: '"http://127.0.0.1:8004/"'
+  BASE_API: '"http://10.7.94.150:8004/"'
 })
